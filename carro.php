301c6ee835e205fab225f6cc5e7690d5bc6f13e9
Diego Herrera <diego@diego-herrera.gateway.2wire.net>
2016-05-30 22:14:08 -0500

    se le agrego bootstrap

--- carro.php
+++ carro.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+    <title></title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" media="screen" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" media="screen" href="css/bootstrap-admin-theme.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
 <?php
 session_start();
 include "php/conexion.php";
@@ -32,3 +43,6 @@ if ($_SESSION['contador'] > 0) {
     <?php
 }
 ?>
+
+</body>
+</html>
