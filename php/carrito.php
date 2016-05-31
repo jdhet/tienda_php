83a2aeb7877f405ebd2882708a556a0ecd20258b
Juan Arturo Herrera Trinidad <cripto1989@gmail.com>
2016-05-30 21:52:09 -0500

    Agregando productos a carro de compra

--- php/carrito.php
+++ php/carrito.php
@@ -4,6 +4,7 @@ function agregarCarro($id_producto,$cantidad) {
     $_SESSION['id'][$_SESSION['contador']] = $id_producto;
     $_SESSION['cantidad'][$_SESSION['contador']] = $cantidad;
     $_SESSION['contador']++;
+    echo "Exito";
 }
 agregarCarro($_POST['id_producto'],$_POST['cantidad']);
 ?>
